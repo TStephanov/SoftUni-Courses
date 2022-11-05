2fb775fcc5fa1a1911da704e928580ddf7e691ac
TStephanov <asgard.battle@gmail.com>
2022-11-05 11:31:21 +0200

    Replace a file

--- JS/Fund/ExamPrep/Class/03P!rates.js
+++ JS/Fund/ExamPrep/Class/03P!rates.js
@@ -4,8 +4,12 @@ function pirates(input) {
     let index = 0;
     let combo = workArr[index]; index++;
 
-    
-    /* // create functions for each action
+    let commands = {
+        Plunder,
+        Prosper
+    }
+
+    // create functions for each action
     function Plunder(p1, p2, p3) {
         let city = p1;
         let kills = Number(p2);
@@ -26,15 +30,14 @@ function pirates(input) {
         let city = p1;
         let goldAdded = Number(p2);
 
-        if (goldAdded < 0) {
-            console.log('Gold added cannot be a negative number!');
-        } else {
+        if (goldAdded > 0) {
             cities[city].gold += goldAdded;
             console.log(`${goldAdded} gold added to the city treasury. ${city} now has ${cities[city].gold} gold.`);
+        } else {
+            console.log('Gold added cannot be a negative number!');
         }
     }
-    */
-
+    
     // parse input until 'Sail' and store cities info
     while (combo !== 'Sail') {
         let [city, pop, gold] = combo.split('||');
@@ -58,43 +61,21 @@ function pirates(input) {
     // modify cities
     while (combo !== 'End') {
         let [action, cityName, p1, p2] = combo.split('=>');
+        let command = commands[action];
 
-        if (action === 'Plunder') {
-            // Plunder=>{town}=>{people}=>{gold}
-            let kills = Number(p1);
-            let gold = Number(p2);
-
-            cities[cityName].pop -= kills;
-            cities[cityName].gold -= gold;
-
-            console.log(`${cityName} plundered! ${gold} gold stolen, ${kills} citizens killed.`);
-
-            if (cities[cityName].pop === 0 || cities[cityName].gold === 0) {
-                delete cities[cityName];
-                console.log(`${cityName} has been wiped off the map!`);
-            }
-
-        } else if (action === 'Prosper') {
-            // Prosper=>{town}=>{gold}
-            let addedGold = Number(p1);
-
-            if (addedGold > 0) {
-                cities[cityName].gold += addedGold;
-                console.log(`${addedGold} gold added to the city treasury. ${cityName} now has ${cities[cityName].gold} gold.`);
-            } else {
-                console.log('Gold added cannot be a negative number!');
-            }
-        }
+        command(cityName, p1, p2);
 
         combo = workArr[index]; index++;
     }
 
     // print
-    if (Object.entries(cities).length > 0) {
-        console.log(`Ahoy, Captain! There are ${Object.entries(cities).length} wealthy settlements to go to:`);
-        for (let [cityName, cityInfo] of Object.entries(cities)) {
-            console.log(`${cityName} -> Population: ${cityInfo.pop} citizens, Gold: ${cityInfo.gold} kg`)
+    if (Object.keys(cities).length > 0) {
+        console.log(`Ahoy, Captain! There are ${Object.keys(cities).length} wealthy settlements to go to:`);
+        for (let city of Object.keys(cities)) {
+            console.log(`${city} -> Population: ${cities[city].pop} citizens, Gold: ${cities[city].gold} kg`);
         }
+    } else {
+        console.log('Ahoy, Captain! All targets have been plundered and destroyed!');
     }
 }
 
