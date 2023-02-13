b0d59e2c5311c0f8ef2d93d0ef65e66d1b236b61
Tsvetomir Iliyanov Stefanov <tstefanov@yettel.bg>
2023-02-13 16:11:59 +0200

    Add new solution

--- JS/Adv/05. JS-Advanced-DOM-Events/01Lab/03. Add-Delete/app.js
+++ JS/Adv/05. JS-Advanced-DOM-Events/01Lab/03. Add-Delete/app.js
@@ -1,3 +1,24 @@
 function addItem() {
-    //TODO...
+    // take ul element
+    let listElements = document.getElementById('items');
+
+    // take user input
+    let userInputElement = document.getElementById('newItemText');
+
+    // create li element with a element - [Delete]
+    let newLiElement = document.createElement('li');
+    newLiElement.textContent = userInputElement.value;
+    userInputElement.value = ''; // empty the input field uppon each click of 'Add' button
+
+    let newInsideAElement = document.createElement('a');
+    newInsideAElement.textContent = '[Delete]';
+    newInsideAElement.href = '#';
+    newInsideAElement.addEventListener('click', () => {
+        newInsideAElement.parentNode.parentNode.removeChild(newInsideAElement.parentNode);
+    });
+
+    newLiElement.appendChild(newInsideAElement);
+
+    // append newly created element to the list
+    listElements.appendChild(newLiElement);
 }
