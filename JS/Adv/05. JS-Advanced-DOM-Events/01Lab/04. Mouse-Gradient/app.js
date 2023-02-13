d40e494dfe618ccf8053961b34b71635d4e1da72
Me4ok <asgard.battle@gmail.com>
2023-02-13 23:21:25 +0200

    Add/update solutions

--- JS/Adv/05. JS-Advanced-DOM-Events/01Lab/04. Mouse-Gradient/app.js
+++ JS/Adv/05. JS-Advanced-DOM-Events/01Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,30 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    let gradientBoxElement = document.getElementById('gradient-box');
+    let resultElement = document.getElementById('result');
+
+    gradientBoxElement.addEventListener('mousemove', (e) => {
+        console.log(e);
+
+        let boxWidth = e.target.offsetWidth;
+        let currentPosition = e.offsetX;
+        let percentage = Math.floor((currentPosition / boxWidth) * 100);
+
+        resultElement.textContent = `${percentage}%`;
+    });
 }
+
+/*
+// Ivo Papazov
+function attachGradientEvents() {
+    let gradientElement = document.getElementById('gradient');
+    let resultElement = document.getElementById('result');
+
+    const gradientMouseoverHandler = (e) => {
+        let percent = Math.floor((e.offsetX / e.target.offsetWidth) * 100);
+
+        resultElement.textContent = `${percent}%`;
+    };
+    
+    gradientElement.addEventListener('mousemove', gradientMouseoverHandler);
+}
+*/
--- JS/Adv/05. JS-Advanced-DOM-Events/01Lab/05. Highlight-Active/app.js
+++ JS/Adv/05. JS-Advanced-DOM-Events/01Lab/05. Highlight-Active/app.js
@@ -1,3 +1,63 @@
 function focused() {
-    console.log('TODO:...');
+    let divElements = document.querySelectorAll('div div');
+
+    for (let div of divElements) {
+        let inputElement = div.querySelector('input[type="text"]');
+        
+        inputElement.addEventListener('mousemove', (e) => {
+            inputElement.parentNode.classList.remove('blurred');
+            inputElement.parentNode.classList.add('focused');
+        });
+        
+        inputElement.addEventListener('mouseout', (e) => {
+            inputElement.parentNode.classList.remove('focused');
+            inputElement.parentNode.classList.add('blurred');
+        });
+    }
 }
+
+document.body.innerHTML = `
+<div>
+        <div>
+            <h1>Section 1</h1><input type="text" />
+        </div>
+        <div>
+            <h1>Section 2</h1><input type="text" />
+        </div>
+        <div>
+            <h1>Section 3</h1><input type="text" />
+        </div>
+        <div>
+            <h1>Section 4</h1><input type="text" />
+        </div>
+    </div>
+`;
+
+result();
+
+// Setup events
+let focusEvent = document.createEvent('UIEvents');
+focusEvent.initEvent('focus', true, true);
+let blurEvent = document.createEvent('UIEvents');
+blurEvent.initEvent('blur', true, true);
+
+// Focus second div
+let allDivs = document.querySelectorAll('div div');
+let div = allDivs[1];
+
+div.querySelector('input').dispatchEvent(focusEvent);
+
+expect(allDivs[0].className).to.not.contains('focused', "Wrong div was highlighted");
+expect(allDivs[1].className).to.contains('focused', "Div was not highlighted");
+expect(allDivs[2].className).to.not.contains('focused', "Wrong div was highlighted");
+expect(allDivs[3].className).to.not.contains('focused', "Wrong div was highlighted");
+
+// Blur second and focus fourth div
+div.querySelector('input').dispatchEvent(blurEvent);
+div = allDivs[3];
+div.querySelector('input').dispatchEvent(focusEvent);
+
+expect(allDivs[0].className).to.not.contains('focused', "Wrong div was highlighted");
+expect(allDivs[1].className).to.not.contains('focused', "Wrong div was highlighted");
+expect(allDivs[2].className).to.not.contains('focused', "Wrong div was highlighted");
+expect(allDivs[3].className).to.contains('focused', "Div was not highlighted");
