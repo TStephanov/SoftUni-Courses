22a21ea3bc5361655c2df5597dc5e1799f2a5f79
Tsvetomir Iliyanov Stefanov <tstefanov@yettel.bg>
2023-02-14 08:29:48 +0200

    Update solution for 04. Mouse Gradient

--- JS/Adv/05. JS-Advanced-DOM-Events/01Lab/04. Mouse-Gradient/app.js
+++ JS/Adv/05. JS-Advanced-DOM-Events/01Lab/04. Mouse-Gradient/app.js
@@ -1,11 +1,17 @@
 function attachGradientEvents() {
-    let gradientBoxElement = document.getElementById('gradient-box');
+    let gradientBoxElement = document.getElementById('gradient');
     let resultElement = document.getElementById('result');
 
     gradientBoxElement.addEventListener('mousemove', (e) => {
-        console.log(e);
+        // console.log(e);
+        // console.log(`e.target.offsetLeft: ${e.target.offsetLeft}`);
+        // console.log(`e.target.clientWidth: ${e.target.clientWidth}`);
+        // console.log(`e.offsetX: ${e.offsetX}`);
+        // console.log('------------');
+        // console.log(`e.target.clientWidth: ${e.target.clientWidth}`);
+        // console.log(`e.target.offsetWidth: ${e.target.offsetWidth}`);
 
-        let boxWidth = e.target.offsetWidth;
+        let boxWidth = e.target.clientWidth;
         let currentPosition = e.offsetX;
         let percentage = Math.floor((currentPosition / boxWidth) * 100);
 
