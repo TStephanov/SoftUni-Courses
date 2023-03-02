e9f00f8c53760d14b89367102ce4a3b1eff1bdbd
TStephanov <asgard.battle@gmail.com>
2023-03-02 17:39:39 +0200

    New files from personal PC

--- JS/Adv/Exam-Prep/01 JS Advanced Retake Exam - 7 December 2022/01. Hotel-Reservation/01. Hotel reservation-Resources/app.js
+++ JS/Adv/Exam-Prep/01 JS Advanced Retake Exam - 7 December 2022/01. Hotel-Reservation/01. Hotel reservation-Resources/app.js
@@ -1,6 +1,26 @@
 window.addEventListener('load', solve);
 
 function solve() {
+    // take all possible elements
+    const fNameEl = document.getElementById('first-name');
+    const lNameEl = document.getElementById('last-name');
+    const fromDateEl = document.getElementById('date-in');
+    const toDateEl = document.getElementById('date-out');
+    const peopleCountEl = document.getElementById('people-count');
+    const nextBtnEl = document.querySelector('button[id="next-btn"]');
+    let reservationInfoListEl = document.getElementsByClassName('info-list')[0];
+    let reservationConfirmListEl = document.getElementsByClassName('confirm-list')[0];
+    console.log(nextBtnEl);
+    nextBtnEl.addEventListener('click', (e) => {
+        console.log(e);
+        // console.log(fNameEl);
+        // console.log(lNameEl);
+        // console.log(fromDateEl);
+        // console.log(toDateEl);
+        // console.log(peopleCountEl);
+        // console.log(nextBtnEl);
+    });
+
     // add reservation info
     // - check flds for corret input
     // - activate "Next" button
@@ -10,8 +30,3 @@ function solve() {
     // confirm/cancel reservation
     
 }
-
-
-
-
-
