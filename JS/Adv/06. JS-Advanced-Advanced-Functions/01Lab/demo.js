a68e137fdee4251c92e3920c009c1f8180814914
TStephanov <asgard.battle@gmail.com>
2023-03-09 12:15:13 +0200

    Update solutions

--- JS/Adv/06. JS-Advanced-Advanced-Functions/01Lab/demo.js
+++ JS/Adv/06. JS-Advanced-Advanced-Functions/01Lab/demo.js
@@ -1,20 +1,5 @@
 function demo() {
-    // Variation
-    const calc = (number) => sum(10, number);
-
-    // Variation
-    const calc2 = function (number) {
-        return sum(13, number)
-    };
-
-    // NOTE: use one of 2 returns, for result
-    return calc;
-    // return calc2;
+    
 }
 
-function sum(a, b) {
-    return a + b;
-}
-
-let result = demo();
-console.log(result(5)); // calc = 15; calc2 = 18
+demo();
