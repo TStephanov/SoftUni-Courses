0756965f450c155e9036bb070e4b47edeb25f169
TStephanov <asgard.battle@gmail.com>
2023-03-16 19:19:27 +0200

    enrich 06.JS-Advanced-Advanced-Functions\02Exercise\05functionalSum.js explanation

--- JS/Adv/06. JS-Advanced-Advanced-Functions/02Exercise/05functionalSum.js
+++ JS/Adv/06. JS-Advanced-Advanced-Functions/02Exercise/05functionalSum.js
@@ -37,3 +37,28 @@ function add(input) {
 // console.log(add(1).toString());
 console.log(add(1)(6)(-3).toString());
 */
+
+/*
+// Other example
+function outer() {
+    let result = 0;
+
+    function demo() {
+        result = 1 + 2;
+
+        return demo;
+    }
+    
+    demo.print = () => {
+        return result;
+    }
+
+    return demo;
+}
+
+const value = outer(); // hold demo function
+const value2 = value(); // hold demo func ... By call value() update result to 1+2 then demo func is returned
+console.log(value.toString());
+console.log(value.print()); // 3 - both prints have result from calling demo.print() - where print is a method of the function demo()
+console.log(value2.print()); // 3 - both prints have result from calling demo.print() - where print is a method of the function demo()
+*/
--- JS/Adv/06. JS-Advanced-Advanced-Functions/02Exercise/demo.js
+++ JS/Adv/06. JS-Advanced-Advanced-Functions/02Exercise/demo.js
@@ -1,16 +1,21 @@
-function add(input) {
-    let sum = 0;
+function outer() {
+    let result = 0;
 
-    function inner(value) {
-        sum += value;
-        return inner;
+    function demo() {
+        result = 1 + 2;
+
+        return demo;
     }
-    inner.toString = () => {
-        return sum;
+    
+    demo.print = () => {
+        return result;
     }
 
-    return inner(input);
+    return demo;
 }
 
-// console.log(add(1).toString());
-console.log(add(1)(6)(-3).toString());
+const value = outer(); // hold demo function
+const value2 = value(); // hold demo func ... By call value() update result to 1+2 then demo func is returned
+console.log(value.toString());
+console.log(value.print()); // 3 - both prints have result from calling demo.print() - where print is a method of the function demo()
+console.log(value2.print()); // 3 - both prints have result from calling demo.print() - where print is a method of the function demo()
