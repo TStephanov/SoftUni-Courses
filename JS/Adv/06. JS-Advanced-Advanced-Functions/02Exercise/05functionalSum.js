7999b9f8151aa3a29399dd483b83cd8987189e8c
TStephanov <asgard.battle@gmail.com>
2023-03-16 08:14:10 +0200

    update solution 06.JS-Advanced-Advanced-Functions\02Exercise\05functionalSum.js explanation

--- JS/Adv/06. JS-Advanced-Advanced-Functions/02Exercise/05functionalSum.js
+++ JS/Adv/06. JS-Advanced-Advanced-Functions/02Exercise/05functionalSum.js
@@ -7,7 +7,7 @@ function add(num) {
         const temp = inner;
         return inner;
     }
-    // it's like, when giving a value to inner.toString (e.g. (arrow function value result)), we are connecting that result with the calling of the function below, in console.log with '.toString()'. Therefore, when calling it in console.log, we are receiving the sum - which is the result of the arrow func - 'return sum'
+    // it's like, when giving a value to inner.toString (e.g. (arrow function value result)), we are connecting that result with the calling of the function below, in console.log with '.toString()'. Therefore, when calling it in console.log, we are receiving the sum - which is the result of the arrow func - 'return sum'. ... As a result of each call of inner(), we are receiving update of sum and inner func itself(explained above). Therefore, when all calls are finished, e.g. '(1)(6)(-3)', as result of '(-3)', we have 'inner', so console.log looks like this console.log(inner.toString()), which calls this below where the result is 'return sum'
     inner.toString = () => {
         return sum;
     }
@@ -15,8 +15,8 @@ function add(num) {
     return inner(num);
 }
 
-console.log(add(1).toString());
-// console.log(add(1)(6)(-3).toString());
+// console.log(add(1).toString());
+console.log(add(1)(6)(-3).toString());
 
 /*
 // Clean solution
