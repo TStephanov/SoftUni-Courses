7d00b8cbc5f024434d526f13cb70b9dba70ad745
TStephanov <asgard.battle@gmail.com>
2023-03-23 19:53:24 +0200

    Practice files

--- JS/Adv/06. JS-Advanced-Advanced-Functions/02Exercise/09. Central Cinema/solution.js
+++ JS/Adv/06. JS-Advanced-Advanced-Functions/02Exercise/09. Central Cinema/solution.js
@@ -1,70 +1,103 @@
 function solve() {
     let activeMoviesList = document.querySelector('section[id="movies"] ul');
-    let onScrBtn = document.querySelector('form[id="add-new"] button');
-    onScrBtn.addEventListener('click', (e) => {
-        debugger;
-        let p = document.createElement('p');
-        p.textContent = 'clicked';
-        activeMoviesList.appendChild(p);
-        console.log('clicked');
-        console.log(e);
-    });
+    const onScrBtn = document.querySelectorAll('button')[0];
+    onScrBtn.addEventListener('click', myFunc);
 
-    // function addItem(event) {
-    //     console.log('clicked');
-    //     console.log(event);
-    //     const inputFields = document.querySelectorAll('input[type="text"]');
-    //     let validValues = true;
-    //     /**
-    //      for (let field of Array.from(inputFields)) {
-    //          if (field.value === '') {
-    //              validValues = false;
-    //              break;
-    //          }
-    //      };
- 
-    //      if (true) { // validValues && isNaN(Array.from(inputFields)[2].value) === false
-    //          const movie = Array.from(inputFields)[0].value;
-    //          const hall = Array.from(inputFields)[1].value;
-    //          const price = Number(Array.from(inputFields)[2].value);
-             
-    //          let newLi = document.createElement('li');
-             
-    //          let newSpan = document.createElement('span');
-    //          newSpan.textContent = movie;
-    //          newLi.appendChild(newSpan);
-    //          console.log(`newSpan: ${newSpan.textContent}`);
-             
-    //          let newStrong = document.createElement('strong');
-    //          newStrong.textContent = hall;
-    //          newLi.appendChild(newStrong);
-    //          console.log(`newStrong: ${newStrong.textContent}`);
-             
-    //          let newDiv = document.createElement('div');            
-    //          let priceStrong = document.createElement('strong');
-    //          priceStrong.textContent = price.toFixed(2);
-    //          newDiv.appendChild(priceStrong);
-    //          console.log(`priceStrong: ${priceStrong.textContent}`);
-             
-    //          let newInput = document.createElement('input');
-    //          newInput.placeholder = 'Tickets Sold';
-    //          newDiv.appendChild(newInput);
-    //          console.log(newInput);
-             
-    //          let newBtn = document.createElement('button');
-    //          newBtn.addEventListener('click', archive);
-    //          newBtn.textContent = 'Archive';
-    //          newDiv.appendChild(newBtn);
-    //          console.log(`newBtn: ${newBtn.textContent}`);
-             
-    //          // debugger;
-    //          newLi.appendChild(newDiv)
-    //          activeMoviesList.appendChild(newLi);
-    //      }
-    //      */
-    //  }        
+    function myFunc(e) {
+        // let p = document.createElement('p');
+        // p.textContent = 'Yeahhh ! ! ! :)'
+        // activeMoviesList.appendChild(p);
 
-    function archive() {
+        const inputFields = document.querySelectorAll('input[type="text"]');
+        const movie = Array.from(inputFields)[0]; // .value
+        const hall = Array.from(inputFields)[1]; // .value
+        const price = Array.from(inputFields)[2]; // .value
+        let validValues = true;
 
+        // check for all fields having values
+        for (let field of Array.from(inputFields)) {
+            if (field.value === '') {
+                validValues = false;
+                break;
+            }
+        };
+
+        if (validValues && isNaN(Array.from(inputFields)[2].value) === false) { // 
+
+            let newLi = document.createElement('li');
+
+            let newSpan = document.createElement('span');
+            newSpan.textContent = movie.value;
+            newLi.appendChild(newSpan);
+            movie.value = '';
+
+            let newStrong = document.createElement('strong');
+            newStrong.textContent = `Hall: ${hall.value}`;
+            newLi.appendChild(newStrong);
+            hall.value = '';
+
+            let newDiv = document.createElement('div');
+            let priceStrong = document.createElement('strong');
+            priceStrong.textContent = Number(price.value).toFixed(2);
+            newDiv.appendChild(priceStrong);
+            price.value = '';
+
+            let newInput = document.createElement('input');
+            newInput.placeholder = 'Tickets Sold';
+            newDiv.appendChild(newInput);
+            // console.log(newInput);
+
+            let newBtn = document.createElement('button');
+            newBtn.textContent = 'Archive';
+            newBtn.addEventListener('click', archive);
+            newDiv.appendChild(newBtn);
+            newLi.appendChild(newDiv)
+
+            activeMoviesList.appendChild(newLi);
+        }
+        e.preventDefault(); // use it, as default behavior of a button inside a form submits the form, hence pressing it doesn't executes expect change of activeMoviesList, but nulls the customer input only instead(standart form behavior)
     }
+    
+    function archive(e) {
+        const inputFld = e.target.parentNode.querySelector('input');
+        console.log(isNaN(Number(inputFld.value)));
+        if (isNaN(Number(inputFld.value)) === false && inputFld.value !== '') { // "inputFld.value !== ''" because isNaN on empty strint = false
+            let archiveSection = document.querySelector('section[id="archive"] ul');
+            const movieName = e.target.parentNode.parentNode.querySelector('span').textContent;
+            const ticketPrice = e.target.parentNode.parentNode.querySelector('div strong').textContent;
+            const ticketsCount = e.target.parentNode.parentNode.querySelector('input').value;
+            const ticketsPrice = ticketsCount * ticketPrice;
+            
+            let newLi = document.createElement('li');
+
+            let spanName = document.createElement('span');
+            spanName.textContent = movieName;
+            newLi.appendChild(spanName);
+
+            let strongTotalPrice = document.createElement('strong');
+            strongTotalPrice.textContent = `Total amount: ${ticketsPrice.toFixed(2)}`;
+            newLi.appendChild(strongTotalPrice);
+
+            let deleteBtn = document.createElement('button');
+            deleteBtn.textContent = 'Delete';
+            deleteBtn.addEventListener('click', del);
+            newLi.appendChild(deleteBtn);
+
+            archiveSection.appendChild(newLi);
+
+            e.target.parentNode.parentNode.remove(e.target.parentNode.parentNode);
+        }
+    }
+
+    function del(e) {
+        e.target.parentNode.remove(e.target.parentNode);
+    }
+
+    document.querySelector('section[id="archive"] button').addEventListener('click', (e) => {
+        let ulElement = e.target.parentNode.querySelector('ul');
+        while (ulElement.firstChild) {
+            console.log('clicked remove');
+            ulElement.removeChild(ulElement.firstChild);
+        }
+    });
 }
--- JS/Adv/06. JS-Advanced-Advanced-Functions/02Exercise/demo2.js
+++ JS/Adv/06. JS-Advanced-Advanced-Functions/02Exercise/demo2.js
@@ -1,12 +1,13 @@
 function demo() {
-    const btn = document.querySelector('form[id="add-new"] div[id="container"] button');
+    const btn = document.querySelector('button');
     btn.addEventListener('click', addElement);
 
     function addElement(e) {
+        console.log(e.buttons);
         let newP = document.createElement('p');
         newP.textContent = 'Just added this paragraph';
-        console.log(newP, newP.textContent);
-        let div = document.querySelector('section[id="movies"]');
+        // console.log(newP, newP.textContent);
+        let div = document.querySelector('div[class="final"]');
         div.appendChild(newP);
         e.preventDefault();
     }
