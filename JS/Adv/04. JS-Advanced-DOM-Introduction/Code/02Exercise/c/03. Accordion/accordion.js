84a9dae36a7bb649ec12d4879171176d27ba172a
Tsvetomir Stephanov <tstephanov_su@abv.bg>
2023-07-26 23:29:01 +0300

    Test changes

--- JS/Adv/04. JS-Advanced-DOM-Introduction/Code/02Exercise/c/03. Accordion/accordion.js
+++ JS/Adv/04. JS-Advanced-DOM-Introduction/Code/02Exercise/c/03. Accordion/accordion.js
@@ -1,3 +1,17 @@
 function toggle() {
-    console.log('TODO:...');
+    let btnElement = document.querySelector('.button');
+    let textElement = document.getElementById('extra');
+
+    switch (btnElement.textContent) {
+        case 'More': {
+            textElement.style.display = 'block';
+            btnElement.textContent = 'Less';
+        };
+            break;
+        case 'Less': {
+            textElement.style.display = 'none';
+            btnElement.textContent = 'More';
+        };
+            break;
+    }
 }
