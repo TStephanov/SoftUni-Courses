276721390815d8ef79958ccd70cd5975a0c6e0e3
TStephanov <asgard.battle@gmail.com>
2023-08-16 09:23:38 +0300

    New practice file solution

--- JS/Adv/05. JS-Advanced-DOM-Events/01Lab/c/06. Dynamic-Validation/app.js
+++ JS/Adv/05. JS-Advanced-DOM-Events/01Lab/c/06. Dynamic-Validation/app.js
@@ -1,3 +1,15 @@
 function validate() {
-    console.log('TODO:...');
+    let emailInputElement = document.getElementById('email');
+    
+    emailInputElement.addEventListener('change', () => {
+        let pattern = /[a-z]+\@[a-z]+\.[a-z]+/g;
+        let userInput = emailInputElement.value;
+
+        if (pattern.exec(userInput) === null) {
+            emailInputElement.classList.add('error');
+        } else {
+            emailInputElement.value = '';
+            emailInputElement.classList.remove('error');
+        }
+    });
 }
