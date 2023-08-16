b81e6a9418391ee176692fb0482d9a40f512fb7e
Tsvetomir Stephanov <tstephanov_su@abv.bg>
2023-08-17 00:24:27 +0300

    Add a task solution

---
+++ JS/Adv/05. JS-Advanced-DOM-Events/02Exercise/c/01. Sections/script.js
@@ -0,0 +1,16 @@
+function create(words) {
+   let contentElement = document.getElementById('content');
+
+   words.forEach(word => {
+      let newDivElement = document.createElement('div');
+      newDivElement.addEventListener('click', (e) => {
+         e.target.querySelector('p').style.display = 'block';
+      });
+      let newParagraphElement = document.createElement('p');
+      newParagraphElement.textContent = word;
+      newParagraphElement.style.display = 'none';
+      newDivElement.appendChild(newParagraphElement);
+      
+      contentElement.appendChild(newDivElement);
+   });
+}
