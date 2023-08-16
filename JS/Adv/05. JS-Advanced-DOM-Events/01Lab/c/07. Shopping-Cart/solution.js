e4ee23ee226bb2fb51c0ad9e2cb07c8a0e13d7e5
TStephanov <asgard.battle@gmail.com>
2023-08-16 13:39:46 +0300

    Practice files new solution added

--- JS/Adv/05. JS-Advanced-DOM-Events/01Lab/c/07. Shopping-Cart/solution.js
+++ JS/Adv/05. JS-Advanced-DOM-Events/01Lab/c/07. Shopping-Cart/solution.js
@@ -1,3 +1,29 @@
 function solve() {
-   //TODO...
+    let buttonElements = document.getElementsByClassName('add-product');
+    let resultArea = Array.from(document.getElementsByTagName('textarea'))[0];
+    let checkoutButtonElement = document.querySelector('button[class="checkout"]');
+    let totalPrice = 0;
+    let productsList = [];
+
+    Array.from(buttonElements).forEach(button => {
+        button.addEventListener('click', (e) => {
+            let productName = e.target.parentNode.parentNode.querySelector('.product-title').textContent;
+            let productPrice = Number(e.target.parentNode.parentNode.querySelector('.product-line-price').textContent);
+
+            resultArea.textContent += `Added ${productName} for ${productPrice.toFixed(2)} to the cart.\n`;
+            totalPrice += productPrice;
+            if (!productsList.includes(productName)) {
+                productsList.push(productName);
+            };
+        });
+    });
+
+    checkoutButtonElement.addEventListener('click', (e) => {
+        resultArea.textContent += `You bought ${productsList.join(', ')} for ${totalPrice.toFixed(2)}.`;
+        checkoutButtonElement.disabled = 'true';
+
+        Array.from(buttonElements).forEach(button => {
+            button.disabled = 'true';
+        });
+    });
 }
