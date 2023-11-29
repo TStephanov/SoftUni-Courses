4c6084caa823c0143d9274c0d3915707e0df1fcf
TStephanov <asgard.battle@gmail.com>
2023-11-29 22:15:59 +0200

    Practice solutions Classes

---
+++ JS/Adv/08. JS-Advanced-Classes/202311/02Practice/demo.js
@@ -0,0 +1,15 @@
+function demo() {
+    const string1 = 'one';
+    const string2 = 'two';
+    const string3 = 'three';
+    const string4 = '';
+
+    if (string4) {
+        console.log(true);
+    } else { 
+        console.log(false);
+    }
+
+}
+
+demo();
