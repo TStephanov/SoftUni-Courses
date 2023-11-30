ec7f6477078c4a8a865b5ba22bc973e62c44e5af
TStephanov <asgard.battle@gmail.com>
2023-11-30 13:41:08 +0200

    Practice solution 07-class-hes.js - "Classes"

---
+++ JS/Adv/08. JS-Advanced-Classes/202311/02Practice/07-class-hex.js
@@ -0,0 +1,38 @@
+class Hex {
+    // Convert decimal to hexadecimal => hexString = number.toString(16);
+    // Convert hexadecimal to decimal => number = parseInt(hexString, 16);
+    
+    constructor(value) {
+        this.value = value;
+    }
+
+    valueOf() {
+        return this.value;
+    }
+
+    toString() {
+        return '0x' + this.value.toString(16).toUpperCase();
+    }
+
+    plus(number) {
+        return new Hex(this.value + number);
+    }
+
+    minus(number) {
+        return new Hex(this.value - number);
+    }
+
+    parse(string) {
+        return parseInt(string, 16);
+    }
+}
+
+let FF = new Hex(255);
+console.log(FF.toString());
+// console.log(FF.valueOf() + 1);
+FF.valueOf() + 1 == 256;
+let a = new Hex(10);
+let b = new Hex(5);
+console.log(a.plus(b).toString());
+console.log(a.plus(b).toString()==='0xF');
+console.log(FF.parse('AAA'));
