0a2ad84e49b407c9c79f14efd04bfa4f882e3f2b
TStephanov <asgard.battle@gmail.com>
2024-01-17 16:55:42 +0200

    Practice - DOM Introduction 2

--- JS/Adv/Exam-Prep/04-JS-Advanced-DOM-Introduction/02Practice/04. Search in List/search.js
+++ JS/Adv/Exam-Prep/04-JS-Advanced-DOM-Introduction/02Practice/04. Search in List/search.js
@@ -1,3 +1,19 @@
 function search() {
-   // TODO
+   const towns = document.querySelectorAll('ul#towns li');
+   const inputElement = document.querySelector('#searchText');
+   const textResult = document.getElementById('result');
+   let counter = 0;
+
+   Array.from(towns)
+      .forEach(town => {
+         if (town.textContent.includes(inputElement.value)) {
+            town.style.fontWeight = 'bold';
+            counter++;
+         } else {
+            // town.style.fontWeight = ''; // leaves empty style of the element
+            town.removeAttribute('style'); // remove the style attribute completely
+         }
+      });
+
+   textResult.textContent = `${counter} matches found`;
 }
--- JS/Adv/Exam-Prep/04-JS-Advanced-DOM-Introduction/02Practice/07. Hell's Kitchen/app.js
+++ JS/Adv/Exam-Prep/04-JS-Advanced-DOM-Introduction/02Practice/07. Hell's Kitchen/app.js
@@ -1,8 +1,47 @@
 function solve() {
-   document.querySelector('#btnSend').addEventListener('click', onClick);
+    document.querySelector('#btnSend').addEventListener('click', onClick);
 
-   function onClick () {
-      //   TODO:
-      
-   }
+    function onClick() {
+        const inputTextElement = document.querySelector('#inputs textarea');
+        const resultBestRestaurant = document.querySelector('#bestRestaurant p');
+        const resultBestRestaurantWorkers = document.querySelector('#workers p');
+        let restaurants = {};
+
+        JSON.parse(inputTextElement.value).forEach(restaurantInfo => {
+            const [restaurantName, emplInfo] = restaurantInfo.split(' - ');
+
+            if (!restaurants.hasOwnProperty(restaurantName)) {
+                restaurants[restaurantName] = {
+                    avgSalary: 0,
+                    bestSalary: 0,
+                    sumSalary: 0,
+                    employees: {},
+                };
+            }
+
+            emplInfo.split(', ').forEach(employee => {
+                let [name, salary] = employee.split(' ');
+                salary = Number(salary);
+                // no need to check if employee already exists for that restaurant
+                restaurants[restaurantName].employees[name] = salary;
+                if (salary > restaurants[restaurantName].bestSalary) {
+                    restaurants[restaurantName].bestSalary = salary;
+                }
+                restaurants[restaurantName].sumSalary += salary;
+                restaurants[restaurantName].avgSalary = restaurants[restaurantName].sumSalary / Object.keys(restaurants[restaurantName].employees).length;
+            });
+        });
+
+        let restaurantsSorted = Object.entries(restaurants).sort((a, b) => b[1].avgSalary - a[1].avgSalary);
+        let bestRestaurantName = restaurantsSorted[0][0];
+        resultBestRestaurant.textContent = `Name: ${bestRestaurantName} Average Salary: ${restaurants[bestRestaurantName].avgSalary.toFixed(2)} Best Salary: ${restaurants[bestRestaurantName].bestSalary.toFixed(2)}`;
+
+        let workersSorted = Object.entries(restaurants[bestRestaurantName].employees).sort((a, b) => b[1] - a[1]);
+        workersSorted.forEach(([name, salary]) => {
+            resultBestRestaurantWorkers.textContent += `Name: ${name} With Salary: ${salary} `;
+        })
+    }
 }
+
+["PizzaHut - Peter 500, George 300, Mark 800", "TheLake - Bob 1300, Joe 780, Jane 660"];
+["Mikes - Steve 1000, Ivan 200, Paul 800", "Fleet - Maria 850, Janet 650"];
