3f8fa0481e33773c1bd52edbf4f93bfb66f33b23
TStephanov <tstephanov_su@abv.bg>
2024-01-18 19:13:29 +0200

    GIT practicing

--- JS/Adv/Exam-Prep/04-JS-Advanced-DOM-Introduction/02Practice/09. Number Convertor/solution.js
+++ JS/Adv/Exam-Prep/04-JS-Advanced-DOM-Introduction/02Practice/09. Number Convertor/solution.js
@@ -1,4 +1,32 @@
 function solve() {
+    const convertBtnElement = document.querySelector('button');
+    convertBtnElement.addEventListener('click', clicked);
 
-    //TODO...
+    const toMenuElement = document.querySelector('#selectMenuTo');
+    let optionBinary = document.createElement('option');
+    optionBinary.setAttribute('selected', '');
+    optionBinary.setAttribute('value', 'binary');
+    optionBinary.textContent = 'Binary';
+    toMenuElement.appendChild(optionBinary);
+    let optionHexadecimal = document.createElement('option');
+    optionHexadecimal.setAttribute('selected', '');
+    optionHexadecimal.setAttribute('value', 'hexadecimal');
+    optionHexadecimal.textContent = 'Hexadecimal';
+    toMenuElement.appendChild(optionHexadecimal);
+
+    function clicked() {
+        let userInputElement = document.getElementById('input');
+        let resultElement = document.getElementById('result');
+
+        switch (toMenuElement.value) {
+            case 'binary': {
+                resultElement.value = Number(userInputElement.value).toString(2);
+            };
+                break;
+            case 'hexadecimal': {
+                resultElement.value = Number(userInputElement.value).toString(16).toUpperCase();
+            };
+                break;
+        }
+    }
 }
