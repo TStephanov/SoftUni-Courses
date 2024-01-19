120c410fd1af558727ab53d30fb8e64d969461a2
TStephanov <asgard.battle@gmail.com>
2024-01-19 16:45:59 +0200

    DOM Events - Practice 2

--- JS/Adv/Exam-Prep/05-JS-Advanced-DOM-Events-Lab-Resources/02. Delete-From-Table/app.js
+++ JS/Adv/Exam-Prep/05-JS-Advanced-DOM-Events-Lab-Resources/02. Delete-From-Table/app.js
@@ -1,3 +1,21 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const userInputElement = document.querySelector('input[name="email"]');
+    let tableElements = document.querySelectorAll('tbody tr td:nth-of-type(2n)');
+    let resultTextElement = document.getElementById('result');
+    let deletedElements = 0;
+
+    Array.from(tableElements).forEach(element => {
+        if (element.textContent === userInputElement.value) {
+            element.parentNode.parentNode.removeChild(element.parentNode);
+            deletedElements++;
+        }
+    });
+
+    if (deletedElements != 0) {
+        resultTextElement.textContent = 'Deleted.';
+    } else {
+        resultTextElement.textContent = 'Not found.';
+    }
+
+    userInputElement.value = '';
 }
