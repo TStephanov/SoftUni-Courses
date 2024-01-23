dd01cf93481a79526c17684f02b62e54f5af466b
TStephanov <asgard.battle@gmail.com>
2024-01-23 17:35:46 +0200

    DOM Events - Practice 2

--- JS/Adv/Exam-Prep/05-JS-Advanced-DOM-Events-Lab-Resources/04. Mouse-Gradient/app.js
+++ JS/Adv/Exam-Prep/05-JS-Advanced-DOM-Events-Lab-Resources/04. Mouse-Gradient/app.js
@@ -1,3 +1,28 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    const gradientBoxElement = document.getElementById('gradient');
+    gradientBoxElement.addEventListener('mousemove', position);
+    let resultElement = document.querySelector('#result');
+
+    function position(e) {
+        let percentage = Math.floor(e.offsetX * 100 / e.target.clientWidth);
+        // console.log(percentage);
+        // console.log(`offsetLeft: ${e.target.offsetLeft}`);
+        // console.log(`clientWidth: ${e.target.clientWidth}`)
+        resultElement.textContent = percentage + '%';
+        // console.log(e);
+        // console.log(e.offsetX);
+    }
 }
+
+// function attachGradientEvents() {
+//     let gradientElement = document.getElementById('gradient');
+//     let resultElement = document.getElementById('result');
+
+//     const gradientMouseoverHandler = (e) => {
+//         let percent = Math.floor((e.offsetX / e.target.offsetWidth) * 100);
+
+//         resultElement.textContent = `${percent}%`;
+//     };
+    
+//     gradientElement.addEventListener('mousemove', gradientMouseoverHandler);
+// }
