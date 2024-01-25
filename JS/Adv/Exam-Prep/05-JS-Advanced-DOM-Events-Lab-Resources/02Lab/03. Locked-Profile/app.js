27d2f89b3bccb63add43c35b542d7d486a3287b6
Tsvetomir Stephanov <tstephanov_su@abv.bg>
2024-01-25 22:13:55 +0200

    DOM Events - Practice 7

--- JS/Adv/Exam-Prep/05-JS-Advanced-DOM-Events-Lab-Resources/02Lab/03. Locked-Profile/app.js
+++ JS/Adv/Exam-Prep/05-JS-Advanced-DOM-Events-Lab-Resources/02Lab/03. Locked-Profile/app.js
@@ -1,3 +1,19 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const buttons = document.querySelectorAll('div.profile button');
+
+    Array.from(buttons).forEach(button => {
+        button.addEventListener('click', (e) => {
+            const isChecked = e.target.parentNode.querySelector('input[value="unlock"]').checked;
+
+            if (isChecked) {
+                if (e.target.textContent === 'Show more') {
+                    e.target.parentNode.querySelector('div').style.display = 'block';
+                    e.target.textContent = 'Hide it';
+                } else {
+                    e.target.parentNode.querySelector('div').style.display = 'none';
+                    e.target.textContent = 'Show more';
+                };
+            }
+        });
+    });
 }
