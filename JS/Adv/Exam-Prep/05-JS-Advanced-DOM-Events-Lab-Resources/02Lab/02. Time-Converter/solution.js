d327120bdfc419d81d5befceb20332e893b76af2
TStephanov <asgard.battle@gmail.com>
2024-01-25 17:35:25 +0200

    DOM Events - Practice 6

--- JS/Adv/Exam-Prep/05-JS-Advanced-DOM-Events-Lab-Resources/02Lab/02. Time-Converter/solution.js
+++ JS/Adv/Exam-Prep/05-JS-Advanced-DOM-Events-Lab-Resources/02Lab/02. Time-Converter/solution.js
@@ -1,4 +1,27 @@
 function attachEventsListeners() {
+    const buttonsElements = document.querySelectorAll('input[type="button"]');
+    const unitsToSeconds = {
+        'seconds': 1,
+        'minutes': 60,
+        'hours': 3600,
+        'days': 86400
+    };
 
-    console.log('TODO:...');
+    Array.from(buttonsElements).forEach(button => {
+        button.addEventListener('click', convert);
+    });
+
+    function convert(e) {
+        
+        // convert inupt to seconds;
+        const pressedButtonType = e.target.id;
+        const inputElement = e.target.parentNode.querySelector('input[type="text"]');
+        let inputToSeconds = Number(inputElement.value) * unitsToSeconds[inputElement.id];
+        
+        // document.querySelector('input[id="seconds"]').textContent = 'What';
+        document.querySelector('input[id="seconds"]').value = inputToSeconds / unitsToSeconds['seconds'];
+        document.querySelector('input[id="minutes"]').value = inputToSeconds / unitsToSeconds['minutes'];
+        document.querySelector('input[id="hours"]').value = inputToSeconds / unitsToSeconds['hours'];
+        document.querySelector('input[id="days"]').value = inputToSeconds / unitsToSeconds['days'];
+    };
 }
