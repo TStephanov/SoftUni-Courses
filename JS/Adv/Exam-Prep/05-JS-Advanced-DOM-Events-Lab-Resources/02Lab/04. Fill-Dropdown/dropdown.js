94eabe795f0fde234af64152fd374960efbfb38b
Tsvetomir Stephanov <tstephanov_su@abv.bg>
2024-01-25 22:31:49 +0200

    DOM Events - Practice 8

--- JS/Adv/Exam-Prep/05-JS-Advanced-DOM-Events-Lab-Resources/02Lab/04. Fill-Dropdown/dropdown.js
+++ JS/Adv/Exam-Prep/05-JS-Advanced-DOM-Events-Lab-Resources/02Lab/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,14 @@
 function addItem() {
-    console.log('TODO:...');
+    const dropDownMenu = document.querySelector('div select[id="menu"]');
+    const inputElementText = document.querySelector('#newItemText');
+    const inputElementValue = document.querySelector('#newItemValue');
+
+    if (inputElementText.value.length !== 0 && inputElementValue.value.length !== 0) {
+        let newOption = document.createElement('option');
+        newOption.value = inputElementValue.value;
+        newOption.textContent = inputElementText.value;
+        dropDownMenu.appendChild(newOption);
+        inputElementValue.value = '';
+        inputElementText.value = '';
+    }
 }
