3b661a25223a1b2fd48fe3bb24686044bac822a9
TStephanov <asgard.battle@gmail.com>
2024-01-25 16:30:12 +0200

    DOM Events - Practice 5

--- JS/Adv/Exam-Prep/05-JS-Advanced-DOM-Events-Lab-Resources/02Lab/01. Sections/script.js
+++ JS/Adv/Exam-Prep/05-JS-Advanced-DOM-Events-Lab-Resources/02Lab/01. Sections/script.js
@@ -1,3 +1,15 @@
 function create(words) {
-   console.log('TODO:...');
+   let contentElement = document.getElementById('content');
+
+   words.forEach(word => {
+      let newDiv = document.createElement('div');
+      newDiv.addEventListener('click', (e) => {
+         e.target.querySelector('p').style.display = 'block';
+      });
+      let newP = document.createElement('p');
+      newP.style.display = 'none';
+      newP.textContent = word;
+      newDiv.appendChild(newP);
+      contentElement.appendChild(newDiv);
+   });
 }
