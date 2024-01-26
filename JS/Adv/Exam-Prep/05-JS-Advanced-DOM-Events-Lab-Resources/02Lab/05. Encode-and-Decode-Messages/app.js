d2ed2346bdcff093efaecad9e85f437c8db8c086
TStephanov <asgard.battle@gmail.com>
2024-01-26 12:09:59 +0200

    DOM Events - Practice 9

--- JS/Adv/Exam-Prep/05-JS-Advanced-DOM-Events-Lab-Resources/02Lab/05. Encode-and-Decode-Messages/app.js
+++ JS/Adv/Exam-Prep/05-JS-Advanced-DOM-Events-Lab-Resources/02Lab/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,30 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const buttonsElements = document.querySelectorAll('div button');
+    Array.from(buttonsElements).forEach(button => {
+        if (button.textContent.includes('Encode')) {
+            button.addEventListener('click', encode)
+        } else if (button.textContent.includes('Decode')) {
+            button.addEventListener('click', decode);
+        }
+    });
+
+    function encode(e) {
+        let textInput = e.target.parentNode.querySelector('textarea');
+        textInput.value = textInput.value
+            .split('')
+            .map(el => el.charCodeAt() + 1)
+            .map(el => String.fromCharCode(el))
+            .join('');
+        const outputField = e.target.parentNode.parentNode.querySelector('div:nth-of-type(2) textarea');
+        outputField.textContent = textInput.value;
+        textInput.value = '';
+    }
+
+    function decode(e) {
+        const textInput = e.target.parentNode.querySelector('textarea');
+        textInput.value = textInput.value.split('')
+            .map(el => el.charCodeAt() - 1)
+            .map(el => String.fromCharCode(el))
+            .join('');
+    }
 }
