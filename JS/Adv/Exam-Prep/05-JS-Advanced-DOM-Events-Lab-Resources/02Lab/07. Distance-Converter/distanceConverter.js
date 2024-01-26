47dd189aebade2d55f5e82451ece9ac6cbb7a159
Tsvetomir Stephanov <tstephanov_su@abv.bg>
2024-01-26 22:14:33 +0200

    DOM Events - Practice 10

--- JS/Adv/Exam-Prep/05-JS-Advanced-DOM-Events-Lab-Resources/02Lab/07. Distance-Converter/distanceConverter.js
+++ JS/Adv/Exam-Prep/05-JS-Advanced-DOM-Events-Lab-Resources/02Lab/07. Distance-Converter/distanceConverter.js
@@ -1,3 +1,22 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    const units = {
+        'mm': 1,
+        'cm': 10,
+        'm': 1000,
+        'km': 1000000,
+        'mi': 1609340,
+        'yrd': 914.4,
+        'ft': 304.8,
+        'in': 25.4
+    };
+
+    document.getElementById('convert').addEventListener('click', (e) => {
+        let inputValueElement = document.querySelector('#inputDistance');
+        let inputUnitsElement = document.querySelector('#inputUnits');
+        let outputValueElement = document.querySelector('#outputDistance');
+        let outputUnitsElement = document.querySelector('#outputUnits');
+
+        let inputToMm = Number(inputValueElement.value) * units[inputUnitsElement.value];
+        outputValueElement.value = inputToMm / units[outputUnitsElement.value];
+    });
 }
