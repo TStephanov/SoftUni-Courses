c3223c27a98cdcd86175e6ff414423c7adaa7820
Tsvetomir Stephanov <tstephanov_su@abv.bg>
2024-01-28 21:10:33 +0200

    DOM Events - Practice 11

--- JS/Adv/Exam-Prep/05-JS-Advanced-DOM-Events-Lab-Resources/02Lab/09. JavaScript-Quiz/solution.js
+++ JS/Adv/Exam-Prep/05-JS-Advanced-DOM-Events-Lab-Resources/02Lab/09. JavaScript-Quiz/solution.js
@@ -1,3 +1,57 @@
 function solve() {
-  //TODO...
+    const buttons = document.querySelectorAll('.answer-text');
+    let resultsElement = document.querySelector('#results');
+    let resultsContentElement = document.querySelector('#quizzie #results h1');
+    let correctAnswersCount = 0;
+    Array.from(buttons).forEach(button => button.addEventListener('click', check));
+    const answers = {
+        "#1": 'onclick',
+        "#2": 'JSON.stringify()',
+        "#3": 'A programming API for HTML and XML documents',
+    }
+    // onclick, JSON.stringify(), A programming API for HTML and XML documents
+
+    function check(ev) {
+        const questionEl = ev.target.parentNode.parentNode.parentNode.querySelector('h2');
+        // console.log(ev.target);
+
+        if (questionEl.textContent.includes('#1')) {
+            if (ev.target.textContent === answers['#1']) {
+                correctAnswersCount++;
+            }
+
+            // resultsContentElement.textContent += ev.target.textContent + ', ';
+            // document.querySelector('#quizzie section:nth-of-type(1)').classList.add('hidden');
+            // document.querySelector('#quizzie section:nth-of-type(2)').classList.remove('hidden');
+            document.querySelector('#quizzie section:nth-of-type(1)').style.display = 'none';
+            document.querySelector('#quizzie section:nth-of-type(2)').style.display = 'block';
+
+        } else if (questionEl.textContent.includes('#2')) {
+            if (ev.target.textContent === answers['#2']) {
+                correctAnswersCount++;
+            }
+
+            // resultsContentElement.textContent += ev.target.textContent + ', ';
+            // document.querySelector('#quizzie section:nth-of-type(2)').classList.add('hidden');
+            // document.querySelector('#quizzie section:nth-of-type(3)').classList.remove('hidden');
+            document.querySelector('#quizzie section:nth-of-type(2)').style.display = 'none';
+            document.querySelector('#quizzie section:nth-of-type(3)').style.display = 'block';
+        } else if (questionEl.textContent.includes('#3')) {
+            if (ev.target.textContent === answers['#3']) {
+                correctAnswersCount++;
+            }
+
+            // resultsContentElement.textContent += ev.target.textContent;
+            // document.querySelector('#quizzie section:nth-of-type(3)').classList.add('hidden');
+            document.querySelector('#quizzie section:nth-of-type(3)').style.display = 'none';
+            resultsElement.style.display = 'block';
+
+            if (correctAnswersCount === 3) {
+                resultsContentElement.textContent += '\nYou are recognized as top JavaScript fan!';
+
+            } else {
+                resultsContentElement.textContent += `\nYou have ${correctAnswersCount} right answers`;
+            }
+        }
+    }
 }
--- JS/Adv/Exam-Prep/demo.js
+++ JS/Adv/Exam-Prep/demo.js
@@ -1,15 +1,10 @@
-function sum(param1, param2) {
-    let a = Number(param1);
-    let b = Number(param2);
-    let result = 0;
-
-    for (let i = a; i <= b; i++) {
-        result += i;
-    }
-
-    console.log(result);
+function demo() {
+    const answers = {
+        "Question #1": 'onclick',
+        "Question #2": 'JSON.stringify()',
+        "Question #3": 'A programming API for HTML and XML documents',
+    };
+    console.log(answers['Question #3'].includes('A programming API for HTML and XML documents'));
 }
 
-sum('1', '1');
-sum('-8', '20');
-sum('5', '1');
+demo();
